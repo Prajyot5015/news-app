8bf9f414b77bcda241f1016723107b561966d862
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-07-23 11:37:40 +0530

    Completed News App

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,22 +1,69 @@
-import React , {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Home.css"
+import axios from 'axios'
+import NewsArticle from '../../components/NewsArticle/NewsArticle'
 
 function Home() {
     const [news, setNews] = useState([])
+    const [searchQuery, setSearchQuery] = useState("pune")
 
-    const loadNews = async ()=>{
+    const loadNews = async () => {
+      try{
+        const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2024-07-21&to=2024-07-21&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`);
 
+        setNews(response.data.articles)
+      }
+      catch(error){
+        console.log(error);
+      }
     }
- 
-    useEffect(()=>{
+
+    useEffect(() => {
         loadNews()
     }, [])
 
-  return (
-    <div>
-        <h1>News App</h1>
-    </div>
-  )
+    useEffect(()=>{
+        loadNews();
+    },[searchQuery])
+
+    return (
+        <div>
+            <h1>News App</h1>
+            <input type='text'
+                value={searchQuery}
+                className='search-input'
+                onChange={(e) =>setSearchQuery(e.target.value)}
+            />
+            <div className='news-container'>
+                {
+                    news.map((newsArticle, i) => {
+
+                        const {
+                            author,
+                            title,
+                            description,
+                            url,
+                            urlToImage,
+                            publishedAt,
+                            content
+                        } = newsArticle
+                        return (
+                            <NewsArticle
+                                key={i}
+                                author={author}
+                                title={title}
+                                description={description}
+                                url={url}
+                                urlToImage={urlToImage}
+                                publishedAt={publishedAt}
+                                content={content}
+                            />
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Home
